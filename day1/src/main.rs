792ea8ccad2904f5c0b1c3be28fae6a54f2a4a57
Dane Larsen <dane.t.larsen@gmail.com>
2025-12-01 22:09:24 -0700

    day1 part2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -15,8 +15,11 @@ fn main() {
         let dir = line.chars().next().unwrap();
         let steps: isize = line[1..].parse().unwrap();
         // just in case any instructions rotate the dial more than one full rotation...
+        let whole_rotations = steps / 100;
         let steps = steps % 100;
+        n_zero += whole_rotations;
 
+        let prev_dial = dial;
         match dir {
             'L' => dial = dial - steps,
             'R' => dial = dial + steps,
@@ -25,11 +28,15 @@ fn main() {
 
         if dial < 0 {
             dial = dial + 100;
+            if prev_dial != 0 {
+                n_zero += 1;
+            }
         } else if dial > 99 {
             dial = dial - 100;
-        }
-
-        if dial == 0 {
+            if prev_dial != 0 {
+                n_zero += 1;
+            }
+        } else if dial == 0 {
             n_zero += 1;
         }
     }
